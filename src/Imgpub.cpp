4b2330f4df1e1d680af8339e93e358444d66d29e
Debjoy <sahadebjoy10@gmail.com>
2019-07-28 00:11:59 +0530

    Modified and corrected

--- src/Imgpub.cpp
+++ src/Imgpub.cpp
@@ -21,26 +21,27 @@ cv_bridge::CvImage img_bridge;
 sensor_msgs::Image img_msg;
 
 int main(int argc, char **argv) {
-	VideoCapture Video(1);
+	VideoCapture Video(0);
 	int counter = 0;
 	
 	ros::init(argc, argv, "image_publisher");
 	ros::NodeHandle nh;
 	image_transport::ImageTransport it(nh);
 	image_transport::Publisher pub = it.advertise("/camera/image_raw", 1);
-	image_transport::Publisher pub2 = it.advertise("/camera/camera_info", 1);
+	//image_transport::Publisher pub2 = it.advertise("/camera/camera_info", 1);
 	while(1)
 	{
 		counter++;
 		Video >> frame;
+		imshow("win", frame);
+		waitKey(1);
 		std_msgs::Header header;
-	        header.seq = counter; 
-	        header.stamp = ros::Time::now(); 
-	        img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, frame);
-	        img_bridge.toImageMsg(img_msg); 
+	    header.seq = counter; 
+	    header.stamp = ros::Time::now(); 
+	    img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, frame);
+	    img_bridge.toImageMsg(img_msg); 
         
 		pub.publish(img_msg);
-		pub2.publish(img_msg);
 	}
 	
 	return 0;
